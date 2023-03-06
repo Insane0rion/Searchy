d7c59abc671cf994d30414643dbd777ae8e69fca
Insane0rion <58635530+Insane0rion@users.noreply.github.com>
2023-03-06 12:43:52 +0100

    added requierments.txt + simple refactoring

--- src/engines.py
+++ src/engines.py
@@ -1,18 +1,21 @@
-import requests
+import mechanize
 import lxml
 from bs4 import BeautifulSoup
 from os import system
 from googleapiclient.discovery import build
 
+
 class SearchEngines:
     results = []
 
     @classmethod
-    def display_all(cls,articels) -> None:
-        system('clear')
+    def display_all(cls, articels) -> None:
+        system("clear")
         print(f"{cls.NAME} Results:")
         for articel in articels:
-            print(f"\n\n{articel['title']}\n\n{articel['description']}\n\n{articel['link']}")
+            print(
+                f"\n\n{articel['title']}\n\n{articel['description']}\n\n{articel['link']}"
+            )
             print(f"{'-'*100}")
 
     @classmethod
@@ -23,44 +26,36 @@ class SearchEngines:
             cls.display_all(articels)
             return articels
         else:
-            print("An error occured getting the page please try again later or contact me!")
-            quit()        
+            print(
+                "An error occured getting the page please try again later or contact me!"
+            )
+            quit()
+
 
 class Wikipedia(SearchEngines):
-
     NAME = "Wikipedia.org"
     root_link = "https://de.wikipedia.org/w/index.php?fulltext=Suchen&search="
 
-    # Getting Html if StatusCode != 200 returning False to stop programs
-    @classmethod
-    def get_raw(cls, query) -> bytes | bool:
-        q = requests.get(Wikipedia.root_link + query)
-        if q.status_code != 200:
-            return False
-        # self.check_if_site_single(q.url)
-        return q.content
-
-    def filter( htmlsrc: bytes, amt:int=0) -> list:
-
+    def filter(htmlsrc: bytes, amt: int = 0) -> list:
         # Func To Scrape Data out of HTML Table element
         # Returing Dict with INFO
         def get_articel_data(articel_table):
-            articel_ = {"title": '',
-                        "description": '',
-                        "link": ''}
+            articel_ = {"title": "", "description": "", "link": ""}
             a = articel_table.find("a")
             articel_["title"] = a.get("title")
             articel_["link"] = "https://de.wikipedia.org" + a.get("href")
             del a
-            articel_["description"] = articel_table.find("div", class_="searchresult").text
+            articel_["description"] = articel_table.find(
+                "div", class_="searchresult"
+            ).text
             return articel_
 
         # Empty List to store scraped Articels
         articels = []
-        soup = BeautifulSoup(htmlsrc, 'lxml')
+        soup = BeautifulSoup(htmlsrc, "lxml")
         # Finding all or just the amt releated articels and scrape their Info
         tables = soup.find_all("td", class_="searchResultImage-text")
-        if amt==0:
+        if amt == 0:
             for table in tables:
                 articel = get_articel_data(table)
                 articels.append(articel)
@@ -73,40 +68,45 @@ class Wikipedia(SearchEngines):
                     pass
         articels.reverse()
         return articels
-    
-class DuckDuckGo(SearchEngines):
 
-    NAME = "duckduckgo.com"
+    @classmethod
+    def get_raw(cls, query):
+        br = mechanize.Browser()
+        br.set_handle_robots(False)
+        return br.open(cls.root_link + query).read()
+
+
+class DuckDuckGo(SearchEngines):
+    NAME = "Duckduckgo.com"
     root_link = "https://html.duckduckgo.com/html?q="
 
     @staticmethod
-    def get_raw( query:str):
-        import mechanize
-        br=mechanize.Browser() # Init class
-        br.set_handle_robots(False) # Make Robots Ignore
-        br.addheaders = [('User-agent', 'Firefox')] # Set header
+    def get_raw(query: str):
+        br = mechanize.Browser()  # Init class
+        br.set_handle_robots(False)  # Make Robots Ignore
+        br.addheaders = [("User-agent", "Firefox")]  # Set header
         res = br.open("https://duckduckgo.com/")
         br.select_form("x")
-        br.form['q'] = query
-        res = br.submit() # Enter query
-        return br.response().read() # Returning HTML
+        br.form["q"] = query
+        res = br.submit()  # Enter query
+        return br.response().read()  # Returning HTML
 
     @staticmethod
-    def filter(htmlsrc, amt)-> list:
-        def get_articel_data(table)->dict:
-            _articel = {"title": '',
-                        "description": '',
-                        "link": ''}
+    def filter(htmlsrc, amt) -> list:
+        def get_articel_data(table) -> dict:
+            _articel = {"title": "", "description": "", "link": ""}
             _articel["title"] = table.find("a", class_="result__a").text
             _articel["description"] = table.find("a", class_="result__snippet").text
-            _articel["link"] = "https://"+table.find("a", class_="result__url").text.strip()
+            _articel["link"] = (
+                "https://" + table.find("a", class_="result__url").text.strip()
+            )
             return _articel
-        
+
         articels = []
-        soup = BeautifulSoup(htmlsrc, 'lxml')
+        soup = BeautifulSoup(htmlsrc, "lxml")
         # Finding all or just the amt releated articels and scrape their Info
         tables = soup.find_all("div", class_="links_main links_deep result__body")
-        if amt==0:
+        if amt == 0:
             for table in tables:
                 articel = get_articel_data(table)
                 articels.append(articel)
@@ -120,66 +120,76 @@ class DuckDuckGo(SearchEngines):
 
 # TODO Display Thumbnail?
 class Youtube(SearchEngines):
-
     NAME = "Youtube.com"
 
-    results = [] # List to store search results
-    API_KEY = ''
+    results = []  # List to store search results
+    API_KEY = ""
 
     @property
-    def set_key(cls,key):
+    def set_key(cls, key):
         cls.API_KEY = key
 
     @classmethod
-    def display_all(cls,videos):
+    def display_all(cls, videos):
         print(f"{cls.NAME} Results:")
         for video in videos:
-            print(f"\n\n{video['video_title']}"
-                  f"\nby {video['channel_name']} | Published at the: {video['publishing_date']}"
-                  f"\n\n{video['description']}"
-                  f"\n\n{video['video_link']}")
+            print(
+                f"\n\n{video['video_title']}"
+                f"\nby {video['channel_name']} | Published at the: {video['publishing_date']}"
+                f"\n\n{video['description']}"
+                f"\n\n{video['video_link']}"
+            )
             print(f"{'-'*100}")
 
     @classmethod
-    def get_raw(cls, query,**kwargs):
+    def get_raw(cls, query, **kwargs):
         try:
-            service = build('youtube', 'v3', developerKey=cls.API_KEY) # Building API connection
-            if kwargs: # Checking if amt is specified
-                yt_request = service.search().list(part='snippet', q=query, maxResults=kwargs['amt'])     
+            service = build(
+                "youtube", "v3", developerKey=cls.API_KEY
+            )  # Building API connection
+            if kwargs:  # Checking if amt is specified
+                yt_request = service.search().list(
+                    part="snippet", q=query, maxResults=kwargs["amt"]
+                )
             else:
-                yt_request = service.search().list(part='snippet', q=query, maxResults=20)
-            results = yt_request.execute() # Getting results
-            service.close() # Closing socket
+                yt_request = service.search().list(
+                    part="snippet", q=query, maxResults=20
+                )
+            results = yt_request.execute()  # Getting results
+            service.close()  # Closing socket
             return results["items"]
         except:
-            print("You're API token seems to be not valid! Please check you're settings.ini file.\n"
-            "If this error doesn't stop please issue an ticket on github!")
+            print(
+                "You're API token seems to be not valid! Please check you're settings.ini file.\n"
+                "If this error doesn't stop please issue an ticket on github!"
+            )
             quit()
 
     # Func to sort needed yt data
     @staticmethod
-    def filter(video_data_raw: dict, *args, **kwargs)-> list:
+    def filter(video_data_raw: dict, *args, **kwargs) -> list:
         def get_info(vid_info):
-
-            video = {'video_title':'',
-                      'video_link':'',
-                      'description':'',
-                      'publishing_date':'',
-                      'thumbnail_link':'',
-                      'channel_name':''}
+            video = {
+                "video_title": "",
+                "video_link": "",
+                "description": "",
+                "publishing_date": "",
+                "thumbnail_link": "",
+                "channel_name": "",
+            }
             # TODO Add Channel and Livestream
-            if vid_info['id']['kind'] != 'youtube#video':
+            if vid_info["id"]["kind"] != "youtube#video":
                 return
-            snipp = vid_info['snippet']
-            ids = vid_info['id']
-            video['video_link'] = "https://www.youtube.com/watch?v=" + ids['videoId']
-            video['video_title'] = snipp['title']
-            video['description'] = snipp['description']
-            video['publishing_date'] = snipp['publishedAt']
-            video['thumbnail_info'] = snipp['thumbnails']['default']
-            video['channel_name'] = snipp['channelTitle']
-            return video 
-        
+            snipp = vid_info["snippet"]
+            ids = vid_info["id"]
+            video["video_link"] = "https://www.youtube.com/watch?v=" + ids["videoId"]
+            video["video_title"] = snipp["title"]
+            video["description"] = snipp["description"]
+            video["publishing_date"] = snipp["publishedAt"]
+            video["thumbnail_info"] = snipp["thumbnails"]["default"]
+            video["channel_name"] = snipp["channelTitle"]
+            return video
+
         videos = []
         for item in video_data_raw:
             video = get_info(item)
@@ -188,8 +198,10 @@ class Youtube(SearchEngines):
         videos.reverse()
         return videos
 
+
 def debug():
     pass
 
-if __name__=='__name__':
+
+if __name__ == "__main__":
     debug()
--- src/fh.py
+++ src/fh.py
@@ -1,20 +1,21 @@
-from os import path
+from os.path import realpath
 from pathlib import Path
+
 
 class FileHandler:
     def __init__(self):
         self.root = self._setRoot
-        self.settings_file = Path(self.root + 'settings.ini')
+        self.settings_file = Path(self.root + "settings.ini")
 
     @property
     def _setRoot(self):
-        splitted_path = path.realpath(__file__).split("/")
+        splitted_path = realpath(__file__).split("/")
         for _ in range(2):
             del splitted_path[-1]
         return "/".join(splitted_path) + "/"
 
     def getSettings(self) -> dict | bool:
-        if prove_settings_file:
+        if self.prove_settings_file():
             return self.load_settings()
         else:
             if self.create_settings():
@@ -29,37 +30,43 @@ class FileHandler:
         try:
             with open(self.settings_file, "w") as f:
                 f.writelines(
-                "[Generell Settings]\n# Currently supported languages: de\nLAN = 'de'\nSTANDART_AMT = '20'\nSTANDART_ENGINE = 'duck'\n######################\n[Youtube Settings]\n# Key needed to use the yt engine (Read Docs for more Info)\nAPI_KEY = ''\n######################\n# If you got any questions please\n# don't hesitate hit to an issue or\n# write me on github!"
+                    "[Generell Settings]\n# Currently supported languages: de\nLAN = 'de'\nSTANDART_AMT = '20'\nSTANDART_ENGINE = 'duck'\n######################\n[Youtube Settings]\n# Key needed to use the yt engine (Read Docs for more Info)\nAPI_KEY = ''\n######################\n# If you got any questions please\n# don't hesitate hit to an issue or\n# write me on github!"
                 )
-            print("Settings.ini has been created! If you want to use the Youtube engine please insert you're API-Key")
+            input(
+                "Settings.ini has been created! If you want to use the Youtube engine please insert you're API-Key!"
+            )
             return True
         except:
             return False
 
     def load_settings(self) -> dict:
-        with open(self.settings_file, 'r') as f:
+        with open(self.settings_file, "r") as f:
             splitted_text = f.readlines()
-        raw_settings = () # Tuple to store raw settings lines
+        raw_settings = ()  # Tuple to store raw settings lines
         for line in splitted_text:
-            if line.startswith(('#','[')): # Checking if its necessary
+            if line.startswith(("#", "[")):  # Checking if its necessary
                 pass
             else:
                 raw_settings = (*raw_settings, line)
         settings = {}
         for setting in raw_settings:
-            setting=setting.split("\n")[0]
+            setting = setting.split("\n")[0]
             key = setting.split(" ")[0]
-            value= setting.split("'")[1]
+            value = setting.split("'")[1]
             settings[key] = value
         return settings
 
     def create_settings(self) -> bool:
-        settings = {"General Settings": {"LAN":'de',
-                                         "STANDARD_ENGINE":'duck',
-                                         'STANDARD_AMT':'20'},
-                    "Youtube Settings": {"API_KEY":''}}
+        settings = {
+            "General Settings": {
+                "LAN": "de",
+                "STANDARD_ENGINE": "duck",
+                "STANDARD_AMT": "20",
+            },
+            "Youtube Settings": {"API_KEY": ""},
+        }
         try:
-            with open(self.settings_file, 'w') as f:
+            with open(self.settings_file, "w") as f:
                 for cat, options in settings.items():
                     f.write(f"[{cat}]\n\n")
                     for option, value in options.items():
@@ -67,6 +74,13 @@ class FileHandler:
                     f.write("\n\n")
             return True
         except:
-            print("Error creating settings.ini file...\nRunning with standard parameters but issue an ticket on"
-                  " github if this keeps happening!")
+            print(
+                "Error creating settings.ini file...\nRunning with standard parameters but issue an ticket on"
+                " github if this keeps happening!"
+            )
             return False
+
+
+def init_settings():
+    fh = FileHandler()
+    return fh.getSettings()
